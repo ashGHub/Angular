c26d986c0ac9c9533582409bf4ab68d19bc37145
Ash <ashgoitom@gmail.com>
2017-12-15 14:24:19 +0300

    Add upload image with preview using directive

---
+++ directives/upload image preview/js/upload.controller.js
@@ -0,0 +1,16 @@
+angular
+  .module('app')
+  .controller('upload', function($scope){
+      $scope.fileContent = '';
+      $scope.fileContentPreveiw = '';
+
+      $scope.setContent = function(content, preview){
+        $scope.fileContent = content;
+        $scope.fileContentPreveiw = preview;
+      };
+
+      $scope.uploadImage = function(){
+        //send your data to the server
+        alert("called");
+      };
+  });
